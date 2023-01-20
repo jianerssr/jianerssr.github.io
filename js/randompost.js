3f6e6372083e5a4c77d8a78e57c424a6bae34920
jianerssr <2151153825@qq.com>
2023-01-20 13:34:27 +0800

    Site updated: 2023-01-20 13:34:26

---
+++ js/randompost.js
@@ -0,0 +1,11 @@
+function randomPost() {
+    fetch('/baidusitemap.xml').then(res => res.text()).then(str => (new window.DOMParser()).parseFromString(str, "text/xml")).then(data => {
+        let ls = data.querySelectorAll('url loc');
+        while (true) {
+            let url = ls[Math.floor(Math.random() * ls.length)].innerHTML;
+            if (location.href == url) continue;
+            location.href = url;
+            return;
+        }
+    })
+}
