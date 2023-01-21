9b87093ec8f1d13877c588d03db8f3b2b5036cc5
jianerssr <2151153825@qq.com>
2023-01-21 20:48:38 +0800

    Site updated: 2023-01-21 20:48:37

--- js/custom.js
+++ js/custom.js
@@ -10,3 +10,46 @@ function share() {
 window.onkeydown=e=>{123===e.keyCode&&btf.snackbarShow("开发者模式已打开，请记得遵循GPL协议哟(●'◡'●)")}
 document.body.addEventListener("copy",(e=>{"TEXTAREA"==e.target.tagName&&""==e.target.className||btf.snackbarShow("滴\^o^/复制成功~")}))
 document.body.addEventListener("paste",(e=>{btf.snackbarShow("滴\^o^/粘贴成功~")}))
+
+//控制台样式自定义
+var now1 = new Date();
+function createtime1() {
+    var grt = new Date("01/14/2023 22:00:00"); //此处修改你的建站时间或者网站上线时间
+    now1.setTime(now1.getTime() + 250);
+    var days = (now1 - grt) / 1000 / 60 / 60 / 24;
+    var dnum = Math.floor(days);
+    var ascll = [
+        `欢迎来到jianerssr的个人小屋`,
+        `---便是仙禁春深，御炉香袅，临轩亲试。对天颜咫尺，定然魁甲登高第。`,
+        `
+     ██╗██╗ █████╗ ███╗   ██╗███████╗██████╗ ███████╗███████╗██████╗ 
+     ██║██║██╔══██╗████╗  ██║██╔════╝██╔══██╗██╔════╝██╔════╝██╔══██╗
+     ██║██║███████║██╔██╗ ██║█████╗  ██████╔╝███████╗███████╗██████╔╝
+██   ██║██║██╔══██║██║╚██╗██║██╔══╝  ██╔══██╗╚════██║╚════██║██╔══██╗
+╚█████╔╝██║██║  ██║██║ ╚████║███████╗██║  ██║███████║███████║██║  ██║
+ ╚════╝ ╚═╝╚═╝  ╚═╝╚═╝  ╚═══╝╚══════╝╚═╝  ╚═╝╚══════╝╚══════╝╚═╝  ╚═╝
+`,
+        "jianerssr的个人小屋已经运行",
+        dnum,
+        "天啦!",
+        "©2023 By jianerssr",
+    ];
+    setTimeout(
+        console.log.bind(
+            console,
+            `\n%c${ascll[0]} %c ${ascll[1]} %c ${ascll[2]} %c${ascll[3]}%c ${ascll[4]}%c ${ascll[5]}\n\n%c ${ascll[6]}\n`,
+            "color:#3366ff",
+            "",
+            "color:#3366ff",
+            "color:#3366ff",
+            "",
+            "color:#3366ff",
+            ""
+        )
+    );
+}
+createtime1();
+// 重写console方法
+console.log = function () { };
+console.error = function () { };
+console.warn = function () { };
