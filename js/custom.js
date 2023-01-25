e069560adaf8ef97b70a2d501604d18383b6ecfe
jianerssr <2151153825@qq.com>
2023-01-25 22:37:05 +0800

    Site updated: 2023-01-25 22:37:04

--- js/custom.js
+++ js/custom.js
@@ -53,3 +53,34 @@ createtime1();
 console.log = function () { };
 console.error = function () { };
 console.warn = function () { };
+
+//页脚底部运行时间
+setInterval(() => {
+    // let create_time = Math.round(new Date('2023-01-14 22:00:00').getTime() / 1000); //在此行修改建站时间
+    // 有苹果用户发现safari浏览器不能正常运行，百度了一下发现是格式化的问题，改成下面这种应该就可以了。感谢反馈。
+    let create_time = Math.round(new Date('2023/01/14 22:00:00').getTime() / 1000); //在此行修改建站时间
+    let timestamp = Math.round((new Date().getTime()) / 1000);
+    let second = timestamp - create_time;
+    let time = new Array(0, 0, 0, 0);
+    var nol = function(h) {
+        return h > 9 ? h : '0' + h;
+    }
+    if (second >= 24 * 3600) {
+        time[0] = parseInt(second / (24 * 3600));
+        second %= 24 * 3600;
+    }
+    if (second >= 3600) {
+        time[1] = nol(parseInt(second / 3600));
+        second %= 3600;
+    }
+    if (second >= 60) {
+        time[2] = nol(parseInt(second / 60));
+        second %= 60;
+    }
+    if (second >= 0) {
+        time[3] = nol(second);
+    }
+    let currentTimeHtml = ""
+    currentTimeHtml += ' 本站已安全运行 ' + time[0] + ' 天 ' + time[1] + ' 小时 ' + time[2] + ' 分 ' + time[3] + ' 秒 ';
+    document.getElementById("runtime").innerHTML = currentTimeHtml;
+}, 1000);
