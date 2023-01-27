a7698c6d3cb8d4f92df7db68694ea180c8a8ac5e
jianerssr <2151153825@qq.com>
2023-01-27 13:47:31 +0800

    Site updated: 2023-01-27 13:47:30

--- js/custom.js
+++ js/custom.js
@@ -84,3 +84,29 @@ setInterval(() => {
     currentTimeHtml += ' 本站已安全运行 ' + time[0] + ' 天 ' + time[1] + ' 小时 ' + time[2] + ' 分 ' + time[3] + ' 秒 ';
     document.getElementById("runtime").innerHTML = currentTimeHtml;
 }, 1000);
+
+
+
+//js有一个小问题：就是只要鼠标滚动不论哪里都会响应，即便你滚动的是子元素
+//2022.9.11 已修复，需要jq，请自行引入
+document.getElementById("name-container").setAttribute("style", "display:none");
+var position = $(window).scrollTop();
+$(window).scroll(function () {
+  var scroll = $(window).scrollTop();
+  if (scroll > position) {
+    document.getElementById("name-container").setAttribute("style", "");
+    document.getElementsByClassName("menus_items")[1].setAttribute("style", "display:none!important");
+  } else {
+    document.getElementsByClassName("menus_items")[1].setAttribute("style", "");
+    document.getElementById("name-container").setAttribute("style", "display:none");
+  }
+  position = scroll;
+});
+function scrollToTop(){
+    document.getElementsByClassName("menus_items")[1].setAttribute("style","");
+    document.getElementById("name-container").setAttribute("style","display:none");
+    btf.scrollToDest(0, 500);
+}
+//修复没有弄右键菜单的童鞋无法回顶部的问题
+document.getElementById("page-name").innerText = document.title.split(" | jianerssr的个人小屋")[0];
+
