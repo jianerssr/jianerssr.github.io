3fb6bcf6fb1b49ee16f4517be5aee4c484e72006
jianerssr <2151153825@qq.com>
2023-02-25 22:50:53 +0800

    Site updated: 2023-02-25 22:50:51

---
+++ js/about.js
@@ -0,0 +1,15 @@
+//关于界面吃喝玩乐
+setInterval(function () {
+    const show = document.querySelector('span[data-show]')
+    const next = show.nextElementSibling || document.querySelector('.first-tips')
+    const up = document.querySelector('span[data-up]')
+
+    if (up) {
+      up.removeAttribute('data-up')
+    }
+
+    show.removeAttribute('data-show')
+    show.setAttribute('data-up', '')
+
+    next.setAttribute('data-show', '')
+  }, 2000)
--- js/custom.js
+++ js/custom.js
@@ -107,19 +107,3 @@ function scrollToTop(){
 }
 //修复没有弄右键菜单的童鞋无法回顶部的问题
 document.getElementById("page-name").innerText = document.title.split(" | jianerssr的个人小屋")[0];
-
-//关于界面吃喝玩乐
-setInterval(function () {
-    const show = document.querySelector('span[data-show]')
-    const next = show.nextElementSibling || document.querySelector('.first-tips')
-    const up = document.querySelector('span[data-up]')
-
-    if (up) {
-      up.removeAttribute('data-up')
-    }
-
-    show.removeAttribute('data-show')
-    show.setAttribute('data-up', '')
-
-    next.setAttribute('data-show', '')
-  }, 2000)
